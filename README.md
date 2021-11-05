d1502bd8df7fd2a5bd0fa23aecb08987130fd802
wr <bgci@tutanota.com>
2021-11-05 13:04:56 +0000

    Upload files to ''

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # Pypi-modules-finder
 
-Скрипт для поиска модулей на pypi.org написаный на Python
+Позволяет прямо из терминала искать модули для языка программирования python на сайте pypi.org.
+
+* Использует pyfzf для выбора модуля из предложенных при поиске
+* Работает очень быстро так как все работает через простые GET запросы
+* Отображает команду для установки модуля и дает ссылку на домашнюю страницу модуля
+
+## Установка
+
+```
+git clone https://notabug.org/wr/Pypi-modules-finder
+
+cd Pypi-modules-finder
+
+pip3 install beautifulsoup4
+
+pip3 install requests
+
+pip3 install lxml
+
+pip3 install pyfzf
+```
+
+Далее устанавливаете fzf через ваш пакетный менеджер, у меня это:
+
+```
+sudo pacman -S fzf
+```
+
+И запускаете скрипт:
+
+```
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,81 @@
+import requests
+from bs4 import BeautifulSoup
+from pyfzf.pyfzf import FzfPrompt
+
+HREFX = 'https://pypi.org/search/?q='
+
+PYPIHREF = 'https://pypi.org'
+
+def check(x, y):
+    for i in range(len(y)):
+        if x == y[i]:
+            return i
+            break
+        else:
+            continue
+    return 404 # Ошибка
+
+def get_html(href):
+    resp = requests.get(href)
+    return resp
+
+def main():
+    print('Вас приветствует поисковик модулей с сайта pypi.org!')
+
+    modulename = input('Введите название модуля который хотите найти: ').replace(' ', '+')
+
+    href = f'{HREFX}{modulename}'
+
+    html = get_html(href)
+
+    soup = BeautifulSoup(html.text, 'lxml')
+
+    packages = soup.find('ul', class_='unstyled')
+    
+    lis = packages.find_all('li')
+
+    hrefsofresults = []
+
+    namesofresults = []
+
+    for li in lis:
+        a = li.find('a')
+        ahref = a.get('href')
+        hrefsofresults.append(PYPIHREF + ahref)
+        spanname = a.find('span', class_='package-snippet__name').text
+        namesofresults.append(spanname)
+
+    fzf = FzfPrompt()
+    
+    selectedmodule = fzf.prompt(namesofresults)[0]
+
+    i = check(selectedmodule, namesofresults)
+
+    href = hrefsofresults[i]
+
+    html = get_html(href)
+
+    soup = BeautifulSoup(html.text, 'lxml')
+
+    header_pip_instructions = soup.find('p', class_='package-header__pip-instructions')
+    comand = header_pip_instructions.find('span', id='pip-command').text
+
+    print('\nКомманда для установки:')
+    print(f'==== {comand}\n')
+
+    releasedp = soup.find('p', class_='package-header__date')
+    releaseddate = releasedp.find('time').text
+
+    print('Модуль вышел:')
+    print(f'{releaseddate}\n')
+
+    homepagea = soup.find('a', class_='vertical-tabs__tab vertical-tabs__tab--with-icon vertical-tabs__tab--condensed')
+    homepage = homepagea.get('href')
+
+    print('Ссылка на домашнюю страницу:')
+    print(f'==== {homepage}')
+    
+
+
+if __name__ == '__main__':
+    main()
