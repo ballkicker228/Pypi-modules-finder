f6f927dffd2aa9571633f10017c1e11e13fc764c
wr <bgci@tutanota.com>
2021-11-07 07:39:39 +0000

    Upload files to ''

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from pyfzf.pyfzf import FzfPrompt
+from os import system
 
 HREFX = 'https://pypi.org/search/?q='
 
@@ -20,61 +21,62 @@ def get_html(href):
     return resp
 
 def main():
-    modulename = input('Введите название модуля который хотите найти: ').replace(' ', '+')
+    while True:
+        modulename = input('Введите название модуля который хотите найти: ').replace(' ', '+')
 
-    href = f'{HREFX}{modulename}'
+        href = f'{HREFX}{modulename}'
 
-    html = get_html(href)
+        html = get_html(href)
 
-    soup = BeautifulSoup(html.text, 'lxml')
+        soup = BeautifulSoup(html.text, 'lxml')
 
-    packages = soup.find('ul', class_='unstyled')
-    
-    lis = packages.find_all('li')
+        packages = soup.find('ul', class_='unstyled')
 
-    hrefsofresults = []
+        lis = packages.find_all('li')
 
-    namesofresults = []
+        hrefsofresults = []
 
-    for li in lis:
-        a = li.find('a')
-        ahref = a.get('href')
-        hrefsofresults.append(PYPIHREF + ahref)
-        spanname = a.find('span', class_='package-snippet__name').text
-        namesofresults.append(spanname)
+        namesofresults = []
 
-    fzf = FzfPrompt()
-    
-    selectedmodule = fzf.prompt(namesofresults)[0]
+        for li in lis:
+            a = li.find('a')
+            ahref = a.get('href')
+            hrefsofresults.append(PYPIHREF + ahref)
+            spanname = a.find('span', class_='package-snippet__name').text
+            namesofresults.append(spanname)
 
-    i = check(selectedmodule, namesofresults)
+        fzf = FzfPrompt()
 
-    href = hrefsofresults[i]
+        selectedmodule = fzf.prompt(namesofresults)[0]
 
-    html = get_html(href)
+        i = check(selectedmodule, namesofresults)
 
-    soup = BeautifulSoup(html.text, 'lxml')
+        href = hrefsofresults[i]
 
-    header_pip_instructions = soup.find('p', class_='package-header__pip-instructions')
-    comand = header_pip_instructions.find('span', id='pip-command').text
+        html = get_html(href)
 
-    print('============================\n\nКомманда для установки:')
-    print(f'---- {comand}\n\n============================\n')
+        soup = BeautifulSoup(html.text, 'lxml')
 
-    releasedp = soup.find('p', class_='package-header__date')
-    releaseddate = releasedp.find('time').text
+        header_pip_instructions = soup.find('p', class_='package-header__pip-instructions')
+        comand = header_pip_instructions.find('span', id='pip-command').text
 
-    print('Последнее обновление модуля:')
-    print(f'{releaseddate}\n\n============================\n')
+        print('============================\n\nКомманда для установки:')
+        print(f'---- {comand}\n\n============================\n')
 
-    try:
-        homepagea = soup.find('a', class_='vertical-tabs__tab vertical-tabs__tab--with-icon vertical-tabs__tab--condensed')
-        homepage = homepagea.get('href')
+        releasedp = soup.find('p', class_='package-header__date')
+        releaseddate = releasedp.find('time').text
 
-        print('Ссылка на домашнюю страницу:')
-        print(f'---- {homepage}')
-    except:
-        print('У данного модуля нет домашней страницы!')
+        print('Последнее обновление модуля:')
+        print(f'{releaseddate}\n\n============================\n')
+
+        try:
+            homepagea = soup.find('a', class_='vertical-tabs__tab vertical-tabs__tab--with-icon vertical-tabs__tab--condensed')
+            homepage = homepagea.get('href')
+
+            print('Ссылка на домашнюю страницу:')
+            print(f'---- {homepage}')
+        except:
+            print('У данного модуля нет домашней страницы!')
     
 
 
